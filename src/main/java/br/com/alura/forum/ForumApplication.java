ce6f9dea4a6641dc19a6510d91b6331703aaaa58
Gustavo Henrique Amaro <gustavohenrique.miranda@hotmail.com>
2022-03-23 13:21:10 -0300

    Paginacao

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 
+@EnableSpringDataWebSupport
 @SpringBootApplication
 public class ForumApplication {
 
